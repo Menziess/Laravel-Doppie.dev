27fb236b134a4ae6367b1f253dc270c6d788bc9c
Menzies <Stefan_schenk@hotmail.com>
2016-04-24 21:26:52 +0200

    Updated migrations

--- database/migrations/2016_04_24_105002_create_files_table.php
+++ database/migrations/2016_04_24_105002_create_files_table.php
@@ -14,6 +14,16 @@ class CreateFilesTable extends Migration
 	{
 		Schema::create('files', function (Blueprint $table) {
 			$table->increments('id');
+			$table->integer('user_id')
+                  ->unsigned()
+                  ->nullable();
+
+            // Constraints
+			$table->foreign('user_id')
+				  ->references('id')
+				  ->on('users')
+				  ->onDelete('set null');
+
 			$table->string('original_name');
 			$table->string('original_extension');
 			$table->string('original_mime_type')->nullable();
--- database/migrations/2016_04_24_115001_create_profiles_table.php
+++ database/migrations/2016_04_24_115001_create_profiles_table.php
@@ -5,55 +5,55 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProfilesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('profiles', function (Blueprint $table) {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('profiles', function (Blueprint $table) {
 
-            // Id's
-            $table->increments('id');
-            $table->integer('user_id')
-                  ->unsigned();
-            $table->integer('file_id')
-                  ->unsigned()
-                  ->nullable();
+			// Id's
+			$table->increments('id');
+			$table->integer('user_id')
+				  ->unsigned();
+			$table->integer('file_id')
+				  ->unsigned()
+				  ->nullable();
 
-            // Constraints
-            $table->foreign('user_id')
-                  ->references('id')
-                  ->on('users')
-                  ->onDelete('cascade');
-            $table->foreign('file_id')
-                  ->references('id')
-                  ->on('files')
-                  ->onDelete('set null');
+			// Constraints
+			$table->foreign('user_id')
+				  ->references('id')
+				  ->on('users')
+				  ->onDelete('cascade');
+			$table->foreign('file_id')
+				  ->references('id')
+				  ->on('files')
+				  ->onDelete('set null');
 
-            // Data
-            $table->integer('gender')
-                  ->nullable();
-            $table->timestamp('date_of_birth')
-                  ->nullable();
-            $table->string('latitude')
-                ->nullable();
-            $table->string('longitude')
-                ->nullable();
+			// Data
+			$table->integer('gender')
+				  ->nullable();
+			$table->timestamp('date_of_birth')
+				  ->nullable();
+			$table->string('latitude')
+				->nullable();
+			$table->string('longitude')
+				->nullable();
 
-            // Tokens & statistics
-            $table->timestamps();
-        });
-    }
+			// Tokens & statistics
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('profiles');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('profiles');
+	}
 }

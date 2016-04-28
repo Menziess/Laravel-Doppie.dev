ab20bd3aee51cea0970ac86600ba20253f7b7561
Menzies <Stefan_schenk@hotmail.com>
2016-04-28 22:12:26 +0200

    Catching empty values in profile

--- resources/views/auth/user/profile.blade.php
+++ resources/views/auth/user/profile.blade.php
@@ -8,14 +8,18 @@
 		<div class="card card-block">
 			@if($user && $user->profile)
 			<h4 class="card-title">Profile</h4>
-				<p class="card-text">Name: {{ $user->first_name . ' ' . $user->last_name ?: 'Not set' }}</p>
-				<p class="card-text">Email: {{ $user->email ?: 'Not set' }}</p>
-				<p class="card-text">Gender: {{ $user->profile->gender ?: 'Not set' }}</p>
-				<p class="card-text">Birthday: {{ $user->profile->date_of_birth ?: 'Not set' }}</p>
+				<p class="card-text">Name: {{ $user->first_name . ' ' . $user->last_name ?: '#' }}</p>
+				<p class="card-text">Email: {{ $user->email ?: '#' }}</p>
+				<p class="card-text">Gender: {{ $user->profile->gender ?: '#' }}</p>
+				<p class="card-text">Birthday: {{ $user->profile->date_of_birth ?: '#' }}</p>
 				<p class="card-text">Location:
+				@if($user->profile->latitude)
 					<a href="https://www.google.nl/maps/@"{{ $user->profile->latitude }}","{{ $user->profile->longitude }}",15z?hl=en" target="blank">
 						{{ $user->profile->latitude . ' ' . $user->profile->longitude }}
 					</a>
+				@else
+				#
+				@endif
 				</p>
 				<a href="#" class="btn btn-primary">Don't press!</a>
 			@endif

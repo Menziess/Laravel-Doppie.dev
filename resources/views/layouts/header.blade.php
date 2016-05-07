62de2ec1f693f2f7a89ab33eed1b46cef7878fd8
Menzies <Stefan_schenk@hotmail.com>
2016-05-07 10:20:17 +0200

    Using blade stacks

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,4 @@
 
-<div id="progress" class="padding-top div-centered-large">
-	<a data-toggle="collapse" href="#header" aria-expanded="false" aria-controls="header">
-	<progress class="progress progress-success" value="25" max="100"></progress>
-	</a>
-</div>
 
 <div id="header" class="collapse {{ isset($in) ? 'in' : '' }}">
 	<div class="container fluid">
@@ -34,3 +29,20 @@
 		</div>
 	</div>
 </div>
+
+    <a id="popover" class="btn" href="#" data-content="Popover with data-trigger" rel="popover" data-placement="bottom" data-original-title="Title" data-trigger="hover">Test</a>
+<div id="progress" class="padding-top div-centered-large">
+	<a data-toggle="collapse" href="#header" aria-expanded="false" aria-controls="header">
+	<progress class="progress progress-success" value="25" max="100"></progress>
+	</a>
+</div>
+
+
+@push('scripts')
+<script>
+	jQuery(document).ready(function($) {
+		$('#popover').popover();
+		$('#popover').popover({ trigger: "hover" });
+	});
+</script>
+@endpush

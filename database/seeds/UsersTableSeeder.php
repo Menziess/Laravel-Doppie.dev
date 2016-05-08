eccbae63782278188ceda381f6670126f68447c9
Menzies <Stefan_schenk@hotmail.com>
2016-05-08 11:12:41 +0200

    Added a user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,8 +12,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 50)->create()->each(function($u) {
-	        // $u->posts()->save(factory(App\Post::class)->make());
+        $faker = Faker::create();
+
+        factory(App\User::class, 3)->create()->each(function($u) use ($faker) {
+            $u->first_name = $faker->firstName;
+	        $u->last_name = $faker->lastName;
+            $u->profile->date_of_birth = $faker->dateTimeBetween('-30 year', '-18 year');
+            $u->profile->latitude    = $faker->randomFloat(6, 47, 49);
+            $u->profile->longitude    = $faker->randomFloat(6, 4, 6);
 	    });
     }
 }

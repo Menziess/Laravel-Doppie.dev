4cdb132d4eec9ae86b7d1602edbf9b2936ded279
Menzies <Stefan_schenk@hotmail.com>
2016-05-14 19:55:33 +0200

    New landing page gif

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
 	<div class="row">
-		<h1 class="display-1 hidden-xs-down">Lets grow something beautifull!</h1>
-		<h3 class="hidden-sm-up">Lets grow something beautifull!</h3>
-		<!-- <a href="{{ url('register') }}" class="over round">
+		<h1 class="display-1 hidden-xs-down">Lets build something beautifull!</h1>
+		<h3 class="hidden-sm-up">Lets build something beautifull!</h3>
+		<a href="{{ url('register') }}" class="over round">
 			<img src="{{ asset('img/landing.gif') }}" class="img-fluid center-block" alt="" />
-		</a> -->
+		</a>
 	</div>
 
 @endsection

18f2b9f844976f05bf125eabf92e33e3a602f78c
Menzies <Stefan_schenk@hotmail.com>
2016-05-14 13:09:35 +0200

    Created subjects pages to show related models

---
+++ resources/views/content/user/subjects.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+
+@section('content')
+
+	@if($subject->projects)
+	<div id="projects" class="card">
+		<!-- <img class="card-img-top" data-src="..." alt="Card image cap"> -->
+		<div class="card-block">
+			<h4 class="card-title">Projects</h4>
+			@if($subject->projects->count() > 0)
+
+			<table class="table table-hover table-large text-small text-xs-left">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Picture</th>
+						<th>Name</th>
+						<th>Owner name</th>
+						<th class="hidden-xs-down">Creation Date</th>
+					</tr>
+				</thead>
+			<tbody>
+			@foreach($subject->projects->take(5) as $project)
+				<tr class="clickable-row" data-href="{{ url(Auth::user()->is_admin ? 'admin/project-settings/' . $project->id : $project->getProfileUrl()) }}">
+					<td>{{ $project->id }}</td>
+					<td><img src="{{ asset($project->getPicture()) }}" class="img-circle profile-picture-small" style="width: 50px;" alt="" ></td>
+					<td>
+						{{ $project->getName() }}
+						@if(Auth::user()->is_admin)
+						<br>
+						{!! $project->is_active
+							? '<span class="label label-pill label-success">active</span>'
+							: '<span class="label label-pill label-warning">inactive</span>' !!}
+						@endif
+					</td>
+					<td>{{ $project->getOwnerName() }}</td>
+					<td class="hidden-xs-down">{{ $project->created_at }}</td>
+				</tr>
+			@endforeach
+			</tbody>
+			</table>
+
+			@else
+			No projects found...
+			@endif
+
+		</div>
+	</div>
+	@endif
+
+	@if($subject->organizations)
+	<div id="organizations" class="card">
+		<!-- <img class="card-img-top" data-src="..." alt="Card image cap"> -->
+		<div class="card-block">
+			<h4 class="card-title">Organizations</h4>
+			@if($subject->organizations->count() > 0)
+
+			<table class="table table-hover table-large text-small text-xs-left">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Picture</th>
+						<th>Name</th>
+						<th>Owner name</th>
+						<th class="hidden-xs-down">Creation Date</th>
+					</tr>
+				</thead>
+			<tbody>
+			@foreach($subject->organizations->take(5) as $organization)
+				<tr class="clickable-row" data-href="{{ url(Auth::user()->is_admin ? 'admin/organization-settings/' . $organization->id : $organization->getProfileUrl()) }}">
+					<td>{{ $organization->id }}</td>
+					<td><img src="{{ asset($organization->getPicture()) }}" class="img-circle profile-picture-small" style="width: 50px;" alt="" ></td>
+					<td>
+						{{ $organization->getName() }}
+						@if(Auth::user()->is_admin)
+						<br>
+						{!! $organization->is_active
+							? '<span class="label label-pill label-success">active</span>'
+							: '<span class="label label-pill label-warning">inactive</span>' !!}
+						@endif
+					</td>
+					<td>{{ $organization->getOwnerName() }}</td>
+					<td class="hidden-xs-down">{{ $organization->created_at }}</td>
+				</tr>
+			@endforeach
+			</tbody>
+			</table>
+
+			@else
+			No organizations found...
+			@endif
+
+		</div>
+	</div>
+	@endif
+
+@endsection

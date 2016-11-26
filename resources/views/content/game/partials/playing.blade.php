478e09dd6133081e1151a7e294da14ad52118990
STEFAN-DESKTOP\Stefan <stefan_schenk@hotmail.com>
2016-11-26 20:11:24 +0100

    Hotfix

--- resources/views/content/game/partials/playing.blade.php
+++ resources/views/content/game/partials/playing.blade.php
@@ -1,5 +1,5 @@
 @if ($game->type == 'Hartenjagen')
 	@include('content.game.partials.playing.hartenjagen')
 @elseif ($game->type == 'Klaverjassen')
-	@include('content.game.partials.playing.Klaverjassen')
+	@include('content.game.partials.playing.klaverjassen')
 @endif

81c0665a788a89af6a49da64a5437612b9d80a9e
STEFAN-DESKTOP\Stefan <stefan_schenk@hotmail.com>
2016-11-27 20:20:19 +0100

    Updated game type page

--- resources/views/content/game/partials/gametype.blade.php
+++ resources/views/content/game/partials/gametype.blade.php
@@ -1,20 +1,25 @@
 <div class="card shadow m-t-3">
 	<div class="card-block">
 		<h2 class="card-title">New Game</h2>
-		<p>
-			<small>Created {{ $game->created_at->diffForHumans() }}</small>
-		</p>
-
+		<small>Created {{ $game->created_at->diffForHumans() }}</small>
 		@include('errors.feedback')
 		<form style="display: inline-block;" method="POST" action="{{ url('game/set-type') }}">
 			{!! csrf_field() !!}
 			{{ method_field('PUT') }}
+			<img class="card-img-top"
+				style="width: 3em; margin: 1em;"
+				src="{{ asset('img/games/heart.png') }}"
+				alt="Game type">
 			<input name="type" value="Hartenjagen" hidden="hidden">
 			<button class="btn btn-primary-outline center-block" type="submit">Hartenjagen</button>
 		</form>
 		<form style="display: inline-block;" method="POST" action="{{ url('game/set-type') }}">
 			{!! csrf_field() !!}
 			{{ method_field('PUT') }}
+			<img class="card-img-top"
+				style="width: 3em; margin: 1em"
+				src="{{ asset('img/games/clover.png') }}"
+				alt="Game type">
 			<input name="type" value="Klaverjassen" hidden="hidden">
 			<button class="btn btn-primary-outline center-block" type="submit">Klaverjassen</button>
 		</form>

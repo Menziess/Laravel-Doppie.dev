11ed0bece15030dd5d410e2f4b7cf7235eeb35e4
STEFAN-DESKTOP\Stefan <stefan_schenk@hotmail.com>
2016-12-21 17:27:38 +0100

    Functioning leaderboards

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -26,7 +26,7 @@ class LeaderboardController extends Controller
 				'losers' => array_keys((array) $game->getData('losers'))
 			];
 		});
-		$klaverjassen = $this->sumScores($klaverjassen);
+		$klaverjassen = $this->winLossRatio($klaverjassen);
 
 		$hartenjagen = $games->where("type", Game::HARTENJAGEN);
 		$hartenjagen = $hartenjagen->map(function ($game) {
@@ -35,7 +35,7 @@ class LeaderboardController extends Controller
 				'losers' => array_keys((array) $game->getData('losers'))
 			];
 		});
-		$hartenjagen = $this->sumScores($hartenjagen);
+		$hartenjagen = $this->winLossRatio($hartenjagen);
 
     	return view('content.game.leaderboards', compact('subject', 'links', 'hartenjagen', 'klaverjassen'));
     }
@@ -44,7 +44,7 @@ class LeaderboardController extends Controller
      * Sum scores per user.
      * @param $array
      */
-    private function sumScores($collection) {
+    private function winLossRatio($collection) {
     	$totals = ["winners" => [], "losers" => []];
     	foreach($collection as $item) {
     		$totals["winners"] = array_merge($totals["winners"], $item["winners"]);
@@ -53,6 +53,33 @@ class LeaderboardController extends Controller
     	$totals["winners"] = array_count_values($totals["winners"]);
     	$totals["losers"] = array_count_values($totals["losers"]);
 
-    	return $totals;
+    	$sorted = $this->array_subtract($totals["winners"], $totals["losers"]);
+    	arsort($sorted);
+
+    	foreach($sorted as $playerId => $ratio) {
+    		$sorted[$playerId] = [
+    			User::findOrFail($playerId),
+    			$ratio,
+    			array_key_exists($playerId, $totals["winners"]) ? $totals["winners"][$playerId] : 0,
+    			array_key_exists($playerId, $totals["losers"]) ? $totals["losers"][$playerId] : 0,
+    		];
+    	}
+
+    	return $sorted;
     }
+
+    /**
+     * Subtract array values.
+     * @param  array $a1
+     * @param  array $a2
+     * @return array
+     */
+    private function array_subtract($a1, $a2) {
+		$aRes = $a1;
+		foreach (array_slice(func_get_args(), 1) as $aRay) {
+			foreach (array_intersect_key($aRay, $aRes) as $key => $val) $aRes[$key] -= $val;
+			foreach (array_diff_key($aRay, $aRes) as $key => $val) $aRes[$key] = -$val;
+		}
+		return $aRes;
+	}
 }
--- resources/views/content/game/leaderboards.blade.php
+++ resources/views/content/game/leaderboards.blade.php
@@ -3,42 +3,78 @@
 @section('content')
 
 <div class="container">
-	<div class="card shadow card-block">
-		<div class="row">
-			<h4>Hartenjagen</h4>
-			{{ App\User::find(15)->getName() }}
-			@if(isset($hartenjagen))
-				<div class="col-md-6 col-centered">
-					Wins:
-					@foreach($hartenjagen["winners"] as $winner => $amount)
-						<br />{{ $winner }} : {{ $amount }}
-						<label class="img-circle profile-picture-small" style="cursor: pointer; background-image: url({{ "" }});"></label>
-					@endforeach
-				</div>
-				<div class="col-md-6 col-centered">
-					Losses:
-					@foreach($hartenjagen["losers"] as $winner => $amount)
-						<br />{{ $winner }} : {{ $amount }}
-					@endforeach
-				</div>
-			@endif
+	<div class="row">
+
+		<div class="col-md-6 col-centered">
+			<div class="card shadow card-block">
+				<h4 class="card-title">Hartenjagen</h4>
+				@if(isset($hartenjagen) && count($hartenjagen) > 0)
+					<table class="table table-hover table-striped table-large text-small text-xs-left">
+						<thead>
+							<tr>
+								<th>Rank</th>
+								<th>Picture</th>
+								<th>Name</th>
+								<th class="hidden-xs-down">Ratio</th>
+								<th class="hidden-md-down">Wins</th>
+								<th class="hidden-md-down">Total games</th>
+							</tr>
+						</thead>
+						<tbody>
+						@foreach($hartenjagen as $key => $player)
+							<tr class="clickable-row touchable" data-href="{{ url($player[0]->getProfileUrl()) }}">
+								<td><h3>{{ $key }}</h3></td>
+								<td>
+									<img src="{{ asset($player[0]->getPicture()) }}" class="img-circle profile-picture-small" style="width: 50px;" alt="" >
+								</td>
+								<td>{{ $player[0]->getName() }}</td>
+								<td class="hidden-xs-down">
+									<strong><span class="text-success">{{ round($player[2] / ($player[2] + $player[3]) * 100) }}%</span></strong>
+								</td>
+								<td class="hidden-md-down">{{ $player[2] }}</td>
+								<td class="hidden-md-down">{{ $player[2] + $player[3] }}</td>
+							</tr>
+						@endforeach
+						</tbody>
+					</table>
+				@endif
+			</div>
 		</div>
-		<div class="row">
-			<h4>Klaverjassen</h4>
-			@if(isset($klaverjassen))
-				<div class="col-md-6 col-centered">
-					Wins:
-					@foreach($klaverjassen["winners"] as $winner => $amount)
-						<br />{{ $winner }} : {{ $amount }}
-					@endforeach
-				</div>
-				<div class="col-md-6 col-centered">
-					Losses:
-					@foreach($klaverjassen["losers"] as $winner => $amount)
-						<br />{{ $winner }} : {{ $amount }}
-					@endforeach
-				</div>
-			@endif
+
+		<div class="col-md-6 col-centered">
+			<div class="card shadow card-block">
+				<h4 class="card-title">Klaverjassen</h4>
+				@if(isset($klaverjassen) && count($klaverjassen) > 0)
+					<table class="table table-hover table-striped table-large text-small text-xs-left">
+						<thead>
+							<tr>
+								<th>Rank</th>
+								<th>Picture</th>
+								<th>Name</th>
+								<th class="hidden-xs-down">Ratio</th>
+								<th class="hidden-md-down">Wins</th>
+								<th class="hidden-md-down">Total games</th>
+							</tr>
+						</thead>
+						<tbody>
+						@foreach($klaverjassen as $player)
+							<tr class="clickable-row touchable" data-href="{{ url($player[0]->getProfileUrl()) }}">
+								<td><h3>{{ $key }}</h3></td>
+								<td>
+									<img src="{{ asset($player[0]->getPicture()) }}" class="img-circle profile-picture-small" style="width: 50px;" alt="" >
+								</td>
+								<td>{{ $player[0]->getName() }}</td>
+								<td class="hidden-xs-down">
+									<strong><span class="text-success">{{ round($player[2] / ($player[2] + $player[3]) * 100) }}%</span></strong>
+								</td>
+								<td class="hidden-md-down">{{ $player[2] }}</td>
+								<td class="hidden-md-down">{{ $player[2] + $player[3] }}</td>
+							</tr>
+						@endforeach
+						</tbody>
+					</table>
+				@endif
+			</div>
 		</div>
 	</div>
 </div>

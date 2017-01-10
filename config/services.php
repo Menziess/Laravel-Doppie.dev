afd7ee797c2d56d7b0ccf008ae2aece5e908dc25
STEFAN-DESKTOP\Stefan <stefan_schenk@hotmail.com>
2017-01-10 23:17:51 +0100

    Dynamic callback url

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
 	'facebook' => [
 		'client_id' => env('FB_CLIENT_ID'),
 		'client_secret' => env('FB_CLIENT_SECRET'),
-		'redirect' => env('FB_REDIRECT'),
+		'redirect' => url('/') . env('FB_REDIRECT'),
 	],
 
 	'google' => [

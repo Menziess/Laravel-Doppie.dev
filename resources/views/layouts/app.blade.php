3f68bd1ab8183e58fa4f851e51a7a8274605a9b3
STEFAN-DESKTOP\Stefan <stefan_schenk@hotmail.com>
2017-01-12 22:59:39 +0100

    Apple splash screen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 	<meta name="description" content="Doppie er af group website">
 	<meta name="author" content="Stefan Schenk">
 
-	<link rel="apple-touch-startup-image" href="{{ asset('favicon/apple-touch-icon.png') }}">
+	<link rel="apple-touch-startup-image" href="{{ asset('favicon/apple-splash-icon.png') }}">
 	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
 	<link rel="icon" type="image/png" href="{{ asset('favicon/favicon-32x32.png') }}" sizes="32x32">
 	<link rel="icon" type="image/png" href="{{ asset('favicon/favicon-16x16.png') }}" sizes="16x16">

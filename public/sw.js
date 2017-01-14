e3ee7d038f07b3f8dbcaad21947dcc397889640d
STEFAN-DESKTOP\Stefan <stefan_schenk@hotmail.com>
2017-01-14 20:39:05 +0100

    Caches

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,17 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('menzies').then(cache => {
       return cache.addAll([
-        '/',
-        'offline.html'
+        '/laravel.dev/public/',
+        'offline.html',
+        'favicon.ico',
+
+        'css/styles.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
+        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css',
+        'https://fonts.googleapis.com/css?family=Lato:100,300,400,700',
+        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js',
+        'https://cdn.jsdelivr.net/tether/1.3.2/tether.min.js',
       ])
       .then(() => self.skipWaiting());
     })
@@ -17,17 +26,17 @@ self.addEventListener('activate',  event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).then(function(response) {
-      if (response.status == 404) {
-        return new Response("Whoops, not found");
-      } else {
-        return response;
-      }
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     }).catch(function() {
-      return new Response("Uh oh, that totally failed!");
+      return Response('lol');
     })
-    // caches.match(event.request).then(response => {
-    //   return response || fetch(event.request);
+
+    // fetch(event.request).then(function(response) {
+    //   return response;
+    // }).catch(function() {
+    //   return caches.match('offline.html');
+    //   return new Response("Uh oh, that totally failed!");
     // })
   );
 });

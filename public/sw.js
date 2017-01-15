411dba8f20d1ee67bf0d541ce0abbc0ee7ff4bcb
Menzies <stefan_schenk@hotmail.com>
2017-01-15 12:34:56 +0100

    Serviceworker checks if page exists, then tries to load the page. If this fails, it will present an offline page.

--- public/sw.js
+++ public/sw.js
@@ -4,39 +4,26 @@ self.addEventListener('install', e => {
     caches.open('menzies').then(cache => {
       return cache.addAll([
         '/',
-        'offline.html',
-        'favicon.ico',
-
-        'css/styles.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
-        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css',
-        'https://fonts.googleapis.com/css?family=Lato:100,300,400,700',
-        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js',
-        'https://cdn.jsdelivr.net/tether/1.3.2/tether.min.js',
+        '/laravel.dev/public',
+        './offline.html',
+        './favicon/favicon-16x16.png',
+        './favicon.ico'
       ])
       .then(() => self.skipWaiting());
     })
   )
 });
 
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate',  e => {
+  e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
     }).catch(function() {
-      return Response('lol');
+      return caches.match('offline.html');
     })
-
-    // fetch(event.request).then(function(response) {
-    //   return response;
-    // }).catch(function() {
-    //   return caches.match('offline.html');
-    //   return new Response("Uh oh, that totally failed!");
-    // })
   );
 });

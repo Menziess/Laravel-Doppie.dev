b01b8fa28b73afe8b23a8287ba20df0100122ea0
STEFAN-DESKTOP\Stefan <stefan_schenk@hotmail.com>
2017-01-16 11:47:01 +0100

    Fixed offline page

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', event => {
       return cache.addAll([
         '/sw.js',
         '/js/index.js',
-        // '/offline.html',
+        '/offline.html',
         '/favicon.ico',
         '/img/games/heart-clover.png',
       ])
